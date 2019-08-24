35c32668e9ff38a75adb25ea6a99288d346e3e95
Achmadsetiawan <achmadsetiawannn@gmail.com>
2019-08-24 23:07:17 +0700

    Second Commit

--- app/src/main/java/com/example/barvolume/MainActivity.java
+++ app/src/main/java/com/example/barvolume/MainActivity.java
@@ -3,12 +3,111 @@ package com.example.barvolume;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /* Deklarasi Layout activity_main name */
+    EditText edtWidth, edtHeight, edtLength;
+    Button btnCalculate;
+    TextView tvResult;
+
+    /* Deklarasi String STATE_RESULT guna untuk memnyimpan pada layout Landscape */
+    private static final String STATE_RESULT = "state_result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Inisiasi R.id Layout activity_main menjadi String ada MainActivity */
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+
+        if (savedInstanceState != null) {
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
+    }
+
+    /* Fungsi Menyimpan data yg sudah ada pada mode tampilan Landscape */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_calculate) {
+
+            /* Deklarasi String pada fungsi onClick */
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            /* Proses Validasi inputan kosong dengan setError */
+            if (TextUtils.isEmpty(inputLength)) {
+                isEmptyFields = true;
+                edtLength.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputWidth)) {
+                isEmptyFields = true;
+                edtWidth.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputHeight)) {
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak boleh kosong");
+            }
+
+            /* Proses Validasi inputan tidak boleh Double */
+            Double length = toDouble(inputLength);
+            Double width = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+            if (length == null) {
+                isInvalidDouble = true;
+                edtLength.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (width == null) {
+                isInvalidDouble = true;
+                edtWidth.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (height == null) {
+                isInvalidDouble = true;
+                edtHeight.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            /* Proses Validasi inputan Kosong dan Double */
+            if (!isEmptyFields && !isInvalidDouble) {
+                double volume = length * width * height;
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    /* Proses Validasi nilai Double dengan salah satu nilai menjadi NULL */
+    Double toDouble(String str) {
+        try {
+            return Double.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
